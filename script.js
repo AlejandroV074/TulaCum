67fb4557b5a87e4b565f51ffa56e3e22e063121d
Alejandro Vargas <61713783+AlejandroV074@users.noreply.github.com>
2025-03-17 16:46:57 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function celebrar() {
+  alert("🎊 ¡Gran día para celebrar tu nueva identidad! 🎊");
+  document.body.style.background = "gold";
+  setTimeout(() => {
+    document.body.style.background = "";
+  }, 1000);
+}
